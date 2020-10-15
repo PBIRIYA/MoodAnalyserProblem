7c76030598e3e8a0d050f2e0a4992a18cc51b21a
Priya Biriya <priyabiriya1707@gmail.com>
2020-10-15 11:12:23 +0530

    [Priya] add . create ability

--- UnitTestMoodAnalyser/UnitTest1.cs
+++ UnitTestMoodAnalyser/UnitTest1.cs
@@ -8,12 +8,27 @@ namespace UnitTestMoodAnalyser
             [TestMethod]
             public void GivenMood_ReturnSad()
             {
-                string expected = "SAD";
-                string message = "I am Sad now";
-                MoodAnalyser moodAnalyser = new MoodAnalyser(message);
-                string mood = moodAnalyser.AnalyseMood();
-                Assert.AreEqual(expected, mood);
+            //Arrange
+            string expected = "SAD";
+            string message = "I am Sad now";
+            MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+            //Act
+            string mood = moodAnalyser.AnalyseMood();
+            //Assert
+            Assert.AreEqual(expected, mood);
+            }
 
+            [TestMethod]
+            public void AnalyzeHappyMood()
+            {
+            //Arrange
+            string message = "I am in Any Mood";
+            string expectedMood = "HAPPY";
+            MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+            //Act
+            string actualMood = moodAnalyser.AnalyseMood();
+            //Assert
+            Assert.AreEqual(expectedMood, actualMood);
             }
         }
-    }
+}

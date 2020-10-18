c1f86c7f12aaa1c0a8cbb1757c4ae6398ebffa01
Priya Biriya <priyabiriya1707@gmail.com>
2020-10-18 12:08:06 +0530

    [Priya] refactor changes

--- UnitTestMoodAnalyser/UnitTest1.cs
+++ UnitTestMoodAnalyser/UnitTest1.cs
@@ -7,129 +7,26 @@ namespace UnitTestMoodAnalyser
     public class UnitTestMoodAnalyser
     {
         [TestMethod]
-        public void when_pass_I_am_Sad_should_return_Sad()
+        public void GivenMood_ReturnSad()
         {
-<<<<<<< HEAD
-            //Arrange
-            string msg = "I am in Sad Mood";
-            MoodAnalyser moodAnalyser = new MoodAnalyser(msg);
-            string expectedMood = "SAD";
-<<<<<<< HEAD
-=======
-            //Arrage
-            MoodAnalyser analyser = new MoodAnalyser();
->>>>>>> UC2_HandleNullException
-            //Act
-            string result1 = analyser.AnalyseMood("I am in Sad Mood");
-=======
-            //Act
-            string actualMood = moodAnalyser.AnalyseMood();
->>>>>>> UC3_CustomException
-            //Assert
-            Assert.AreEqual("SAD", result1);
+            string expected = "SAD";
+            string message = "I am Sad now";
+            MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+            string mood = moodAnalyser.AnalyseMood();
+            Assert.AreEqual(expected, mood);
         }
 
         [TestMethod]
-        public void when_pass_I_am_Happy_should_return_happy()
+        public void AnalyzeHappyMood()
         {
-<<<<<<< HEAD
             //Arrange
-            string msg = "I am in Any Mood";
-            MoodAnalyser moodAnalyser = new MoodAnalyser(msg);
+            string message = "I am in Any Mood";
             string expectedMood = "HAPPY";
-<<<<<<< HEAD
-=======
-            //Arrage
-            MoodAnalyser analyser = new MoodAnalyser();
->>>>>>> UC2_HandleNullException
-            //Act
-            string result2 = analyser.AnalyseMood("I am in Any Mood");
-=======
+            MoodAnalyser moodAnalyser = new MoodAnalyser(message);
             //Act
             string actualMood = moodAnalyser.AnalyseMood();
->>>>>>> UC3_CustomException
             //Assert
-            Assert.AreEqual("HAPPY", result2);
-        }
-
-        [TestMethod]
-        public void Given_NULL_Mood_Should_Throw_NullException()
-        {
-            try
-            {
-                string msg = null;
-                MoodAnalyser analyser = new MoodAnalyser();
-                string mood = analyser.AnalyseMood(msg);
-            }
-            catch (Exception e)
-            {
-                Assert.AreEqual("Value cannot be null", e.Message);
-            }
-        }
-
-        [TestMethod]
-        public void AnalyzeNullExceptionHandling()
-        {
-            //Arrange
-            string msg = null;
-<<<<<<< HEAD
-            MoodAnalyzer moodAnalyser = new MoodAnalyzer(msg);
-=======
-            MoodAnalyser moodAnalyser = new MoodAnalyser(msg);
->>>>>>> UC3_CustomException
-            //Act => Assert
-            Assert.ThrowsException<MoodAnalyserCustomException>(() => moodAnalyser.AnalyseMood());
-        }
-
-        [TestMethod]
-        public void AnalyzeNullExceptionMessage()
-        {
-            //Arrange
-            string msg = null;
-            MoodAnalyser moodAnalyser = new MoodAnalyser(msg);
-            string expectedMsg = "NULL";
-            string actualMsg = "";
-            //Act
-            try
-            {
-                actualMsg = moodAnalyser.AnalyseMood();
-            }
-            catch (MoodAnalyserCustomException exception)
-            {
-                actualMsg = exception.Message;
-            }
-            //Assert
-            Assert.AreEqual(expectedMsg, actualMsg);
-        }
-
-        [TestMethod]
-        public void AnalyzeEmptyExceptionHandling()
-        {
-            //Arrange
-            string msg = "";
-            MoodAnalyser moodAnalyser = new MoodAnalyser(msg);
-            //Act => Assert
-            Assert.ThrowsException<MoodAnalyserCustomException>(() => moodAnalyser.AnalyseMood());
-        }
-
-        [TestMethod]
-        public void AnalyzeEmptyExceptionMessage()
-        {
-            //Arrange
-            string msg = "";
-            MoodAnalyser moodAnalyser = new MoodAnalyser(msg);
-            string expectedMsg = "EMPTY";
-            string actualMsg = "";
-            //Act
-            try
-            {
-                actualMsg = moodAnalyser.AnalyseMood();
-            }
-            catch (MoodAnalyserCustomException exception)
-            {
-                actualMsg = exception.Message;
-            }
-            Assert.AreEqual(expectedMsg, actualMsg);
+            Assert.AreEqual(expectedMood, actualMood);
         }
     }
 }
